b8df55bbc22140fa2a79ddd1c6459b42fdb88d2f
emkelsey32 <40522897+emkelsey32@users.noreply.github.com>
2021-01-30 17:41:28 -0500

    Added stylings and form for create-poll

--- frontend/src/app/create-poll/create-poll.component.ts
+++ frontend/src/app/create-poll/create-poll.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-poll',
@@ -7,13 +8,31 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./create-poll.component.scss']
 })
 export class CreatePollComponent implements OnInit {
+  createPollForm: FormGroup;
+  loading: boolean;
+  submitted = false;
 
-  constructor(private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.createPollForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      question: ['', Validators.required],
+      answer1: ['', Validators.required],
+      answer2: ['', Validators.required]
+
+  });
+
   }
 
+  get f() { return this.createPollForm.controls; }
+
+
   goToPolling(): void {
+    this.submitted = true;
+    this.loading = true;
+    console.log(this.createPollForm.value)
     this.router.navigate(['/polling']);
+   
   }
 }
