1c808af0e5013a756ac9c4eca63168a13f35679e
shovere <shovere@ufl.edu>
2021-01-31 00:37:50 -0500

    intermittent server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const admin = require("firebase-admin");
 const serviceAccount = require("./gatormeet-17f79-firebase-adminsdk-ujwcq-28f30968eb.json");
+const FieldValue = require("firebase-admin").firestore.FieldValue;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -48,9 +49,9 @@ app.post("/getMeetings", (req, res) => {
   db.collection("Meetings")
     .where("Name", "==", meetingName)
     .get()
-    .then((data) => {
+    .then((ref) => {
       let temp = []  
-      data.forEach((meeting) => {
+      ref.forEach((meeting) => {
           temp.push(meeting.data().Owner)
       });
       res.send(temp)
@@ -84,15 +85,17 @@ res.send("Owner name not found");
 // use json object to transfer data
 app.post("/create-poll", (req, res) => {
 
-  const question = req.body.question
-  const ownerEmail = req.body.ownerEmail
-  const answerOne = req.body.answer1
-  const answerTwo = req.body.answer2
-  const ID = generateUniqueToken()
+  let question = req.body.question
+  let ownerEmail = req.body.ownerEmail
+  let answerOne = req.body.answer1
+  let answerTwo = req.body.answer2
+  let ID = Math.random().toString(36).substr(2, 8)
  
    db.collection("Meetings")
-    .doc()
-    .set({ Answer1Text: answerOne, Answer2Text: answerTwo, OwnerEmail:ownerEmail, Question : question, UniqueID: ID})
+    .doc(ID)
+    .set({ Answer1Text: answerOne, Answer2Text: answerTwo, 
+      OwnerEmail:ownerEmail, Question : question, UniqueID: ID, 
+      Option1:[], Option2:[], NOTA:[], Total:[]})
     .then(() => {
       res.send("Meeting Created");
     })
@@ -101,81 +104,46 @@ app.post("/create-poll", (req, res) => {
     });
 });
 
-// Create new entries into the database
-
-
-function newMeeting()
+app.post("/postPollChoice", (req, res) => 
 {
-  // include one of the three methods to trigger this function in the front end code
-  // https://stackoverflow.com/questions/1947263/using-an-html-button-to-call-a-javascript-function
-  // ^^ 3 ways to trigger function execution when button pressed
+  const choice = req.body.choice 
+  const email = req.body.email
+  const ID = req.body.ID
 
+  if (choice == 0) {
 
-  // Add a new document in collection "meeting"
-  // https://firebase.google.com/docs/firestore/manage-data/add-data
-  const data = {
-    // How to get values from drop down
-    // https://stackoverflow.com/questions/4029281/get-drop-down-value
-
-
-
-    MeetingName: document.getElementById("DROPDOWNMENU").value,
-    OwnerEmail: document.getElementById("DROPDOWNMENU").value,
-    FirstName: document.getElementById("DROPDOWNMENU").value,//
-    LastName: document.getElementById("DROPDOWNMENU").value,
-    UniqueToken: document.getElementById("DROPDOWNMENU").value,
+    db.collection("Meetings")
+      .doc(ID)
+      .update({ NOTA: FieldValue.arrayUnion(email) });
   }
-  const res = await db.collection("Meetings").doc(TOKEN).set(data);
-}
-// my second time at a commit
-function goToMeeting()
-{
- // https://stackoverflow.com/questions/55777601/how-to-append-an-element-into-array-inside-a-firestore-document
- // How to append to an array
-}
-function pollResult()
-{
-    //https://firebase.google.com/docs/firestore/manage-data/add-data
-  // ^^ How to increment data, ctrl+F increment article section
-
-  const admin = require('firebase-admin');
-  const meetingsRef = db.collection('Meetings').doc(TOKEN).set(data);
-  if(document.getElementById("someButton").value == true)
+  if(choice == 1)
   {
-    const res = await meetingsRef.update(
-    {yesResult : admin.firestore.FieldValue.increment(1)});
-  }
-  else
-  {
-    const res = await meetingsRef.update(
-    {yesResult : admin.firestore.FieldValue.increment(1)});
-  }
- 
-}
+       db.collection("Meetings").doc(ID).get().then((docRef) => {
+      let ls = docRef.data().Option1;
+      ls.forEach((element) => {
+        
+      })
+      });
+      
 
-// Generate unique 8 digit ID with node.js - SHOULD use an already built function to ensure uniqueness
-const crypto = require('crypto');
-function generateUniqueToken()
-{
-  // https://firebase.google.com/docs/firestore/query-data/queries
-  // ^^ guide to simple queries in js for google firestore
-  var token = randomHexValue(8);
-  const meetingsRef = db.collection('Meetings')
-  const queryResult = await meetingsRef.where("UniqueToken", "==", token).get();
-  while(!queryResult.empty)
-  {
-    token = randomHexValue(8);
-    queryResult = await meetingsRef.where("UniqueToken", "==", token).get();
+      db.collection("Meetings")
+        .doc(ID)
+        .update({ Option1:FieldValue.arrayUnion(email) }).then(() => {
+      res.send("Meeting Created");
+    })
+    .catch(() => {
+      res.send("Meeting Creation Failed");
+    });
   }
-  return token;
-}
+  if(choice == 2)
+  {
+      db.collection("Meetings").doc(ID).update({Option2 : FieldValue.arrayUnion(email)})
+  }
+  
+   db.collection("Meetings").doc(ID).update({Total: FieldValue.arrayUnion(email)})
+   
+})
 
-function randomHexValue(len)
-{
-  // https://stackoverflow.com/questions/34386914/generate-unique-random-string-with-javascript
-  // convert to hex
-  return crypto.randomBytes(Math.ceil(len/2)).toString('hex').slice(0,len).toUpperCase(); 
-}
 
 app.listen(port, () => {
     console.log("working")
